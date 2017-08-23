3b257fb15bd02832af2bb20e9ca9fec4757f89bf
Erik C. Thauvin <erik@thauvin.net>
2017-08-22 21:37:28 -0700

    Fixed deprecated calls.

--- app/src/main/java/net/thauvin/erik/android/tesremoteprogrammer/widget/ScrollAwareFABBehavior.kt
+++ app/src/main/java/net/thauvin/erik/android/tesremoteprogrammer/widget/ScrollAwareFABBehavior.kt
@@ -22,25 +22,28 @@ import android.support.design.widget.FloatingActionButton
 import android.support.v4.view.ViewCompat
 import android.view.View
 
-class ScrollAwareFABBehavior() : FloatingActionButton.Behavior() {
-    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout?,
-                                     child: FloatingActionButton?,
-                                     directTargetChild: View?,
-                                     target: View?,
-                                     nestedScrollAxes: Int): Boolean {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
-                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes)
-    }
+class ScrollAwareFABBehavior : FloatingActionButton.Behavior() {
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                     child: FloatingActionButton,
+                                     directTargetChild: View,
+                                     target: View,
+                                     nestedScrollAxes: Int,
+                                     type: Int): Boolean =
+            nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                    super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                            nestedScrollAxes, type)
 
-    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout?,
-                                child: FloatingActionButton?,
-                                target: View?,
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                child: FloatingActionButton,
+                                target: View,
                                 dxConsumed: Int,
                                 dyConsumed: Int,
                                 dxUnconsumed: Int,
-                                dyUnconsumed: Int) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed)
-        if (dyConsumed > 0 && child!!.visibility == View.VISIBLE) {
+                                dyUnconsumed: Int,
+                                type: Int) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,
+                dxUnconsumed, dyUnconsumed, type)
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
             // see: https://stackoverflow.com/a/42082313/5640587
             child.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
                 override fun onHidden(fab: FloatingActionButton?) {
@@ -48,7 +51,7 @@ class ScrollAwareFABBehavior() : FloatingActionButton.Behavior() {
                     fab!!.visibility = View.INVISIBLE
                 }
             })
-        } else if (dyConsumed < 0 && child!!.visibility != View.VISIBLE) {
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
             child.show()
         }
     }

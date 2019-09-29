cb71b2724334533bbbe348eb5f017c65c9c6117d
Erik C. Thauvin <erik@thauvin.net>
2019-09-29 01:11:42 -0700

    Added Firebase analytics.

--- app/src/main/java/net/thauvin/erik/android/tesremoteprogrammer/AboutActivity.kt
+++ app/src/main/java/net/thauvin/erik/android/tesremoteprogrammer/AboutActivity.kt
@@ -20,10 +20,12 @@ package net.thauvin.erik.android.tesremoteprogrammer
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import mehdi.sakout.aboutpage.AboutPage
 import mehdi.sakout.aboutpage.Element
+import net.thauvin.erik.android.tesremoteprogrammer.util.fromHtml
 import java.util.Calendar
 
 /**
@@ -47,16 +49,31 @@ class AboutActivity : AppCompatActivity() {
             setImage(R.drawable.background_splash)
             val version = String.format(getString(R.string.about_version), BuildConfig.VERSION_NAME)
             setDescription(
-                "${version}\n© ${Calendar.getInstance().get(Calendar.YEAR)} Erik C. Thauvin️"
+                "<strong>$version</strong><br><small>&copy; ${Calendar.getInstance().get(Calendar.YEAR)} Erik C. Thauvin</small>️".fromHtml()
             )
             addEmail("erik@thauvin.net")
             addGitHub("ethauvin/TESRemoteProgrammer")
             addWebsite("https://m.thauvin.net/android/TESRemoteProgrammer/licenses.shtml")
             addItem(getPrivacyPolicy())
             addTwitter("ethauvin")
+
+            if (BuildConfig.DEBUG) {
+                addItem(getCrashTest())
+            }
         }.create()
 
         setContentView(aboutPage)
+    }
+
+    private fun getCrashTest(): Element {
+        return Element().apply {
+            title = "Crash Test"
+            iconDrawable = android.R.drawable.ic_dialog_alert
+            iconTint = R.color.about_github_color
+            onClickListener = View.OnClickListener {
+                throw RuntimeException("This is a crash!")
+            }
+        }
     }
 
     private fun getPrivacyPolicy(): Element {
@@ -67,7 +84,7 @@ class AboutActivity : AppCompatActivity() {
             value = "privacy"
             intent = Intent(
                 Intent.ACTION_VIEW,
-                Uri.parse("https://m.thauvin.net/apps-privacy.shtml")
+                Uri.parse("https://m.thauvin.net/android/TESRemoteProgrammer/privacy.shtml")
             )
         }
     }
